47bc4fb44ab9e946954ef937aac0361b0952d4a7
trinhvietky <161665737+trinhvietky@users.noreply.github.com>
2024-10-24 07:45:41 +0700

    commit route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SizeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ColorController;
+use App\Models\Color;
+use App\Models\Size;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,58 +13,11 @@ use App\Http\Controllers\UserController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-<<<<<<< HEAD
-Route::get('/', function () {
-    return view('index');
-})->name('index');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
-
-
-// Các trang khác (ví dụ Blog, About, Contact)
-Route::get('/blog', function () {
-    return view('blog');
-})->name('blog');
-
-Route::get('/blog-detail', function () {
-    return view('blog-detail');
-})->name('blog-');
-
-Route::get('/product', function () {
-    return view('product');
-})->name('product');
-
-Route::get('/product-detail', function () {
-    return view('product-detail');
-})->name('product-');
-
-Route::get('/shoping-cart', function () {
-    return view('shoping-cart');
-})->name('shoping-cart');
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-=======
 Route::get('/admin-home', function () {
     return view('admin-home');
 });
@@ -69,7 +25,7 @@ Route::get('/user-list', function () {
     return view('user-list');
 });
 Route::get('/user-add', function () {
-    return view('user-add');
+    return view('admin.user.user-add');
 });
 Route::get('/user-list', [UserController::class, 'index']);
 
@@ -88,6 +44,60 @@ Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update')
 
 
 Route::get('/', function () {
-    return view('index');
+    return view('page-home');
 });
->>>>>>> main
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
+
+//-------SIZE
+//Hiển thị view size list
+Route::get('/size-list', function () {
+    return view('admin.size.size-list');
+}); 
+
+//Hiển thị danh sách size
+Route::get('/size-list', [SizeController::class, 'index'])->name('size-list');
+
+//Xóa size
+Route::delete('/size/{size_id}', [SizeController::class, 'destroy'])->name('size.destroy');
+Route::get('/size-list', [SizeController::class, 'index'])->name('size-list');
+
+// Add size
+Route::get('/size-add', function () {
+    return view('admin.size.size-add');
+});
+
+Route::post('/sizes', [SizeController::class, 'store'])->name('size.store');
+
+//Edit size
+Route::get('/size/{size_id}/edit', [SizeController::class, 'edit'])->name('size.edit');
+
+Route::put('/size/{id}', [SizeController::class, 'update'])->name('size.update');
+
+
+//Color
+//Hiển thị view color list
+Route::get('/color-list', function () {
+    return view('admin.color.color-list');
+}); 
+
+//Hiển thị danh sách color
+Route::get('/color-list', [ColorController::class, 'index'])->name('color-list');
+
+//Xóa color
+Route::delete('/color/{color_id}', [ColorController::class, 'destroy'])->name('color.destroy');
+Route::get('/color-list', [ColorController::class, 'index'])->name('color-list');
+
+// Add color
+Route::get('/color-add', function () {
+    return view('admin.color.color-add');
+});
+
+Route::post('/colors', [ColorController::class, 'store'])->name('color.store');
+
+//Edit size
+Route::get('/color/{color_id}/edit', [ColorController::class, 'edit'])->name('color.edit');
+
+Route::put('/color/{id}', [ColorController::class, 'update'])->name('color.update');
