3f585d335aeccbb129cc7a54903c75dc110a854b
D-NgocThang <152775141+D-NgocThang@users.noreply.github.com>
2024-11-13 20:16:07 +0700

    update

--- resources/views/shoping-cart.blade.php
+++ resources/views/shoping-cart.blade.php
@@ -73,42 +73,30 @@
 					<!-- Menu desktop -->
 					<div class="menu-desktop">
 						<ul class="main-menu">
-<<<<<<< HEAD
+
 						<li class="active-menu">
 								<a href="{{route('index')}}">Home</a>
 							</li>
 
 							<li>
 								<a href="{{route('product')}}">Shop</a>
-=======
-							<li>
-								<a href="index.html">Home</a>
->>>>>>> main
+
 								<ul class="sub-menu">
 									<li><a href="index.html">Homepage 1</a></li>
 									<li><a href="home-02.html">Homepage 2</a></li>
 									<li><a href="home-03.html">Homepage 3</a></li>
-<<<<<<< HEAD
+
 									<li><a href="home-03.html">Homepage 3</a></li>
 									<li><a href="home-03.html">Homepage 3</a></li>
 								</ul>
 							</li>
 
-=======
-								</ul>
-							</li>
-
-							<li>
-								<a href="product.html">Shop</a>
-							</li>
-
->>>>>>> main
 							<li class="label1" data-label1="hot">
 								<a href="shoping-cart.html">Features</a>
 							</li>
 
 							<li>
-<<<<<<< HEAD
+
 								<a href="{{route('blog')}}">Blog</a>
 							</li>
 
@@ -118,17 +106,7 @@
 
 							<li>
 								<a href="{{route('contact')}}">Contact</a>
-=======
-								<a href="blog.html">Blog</a>
-							</li>
 
-							<li>
-								<a href="about.html">About</a>
-							</li>
-
-							<li>
-								<a href="contact.html">Contact</a>
->>>>>>> main
 							</li>
 						</ul>
 					</div>	
@@ -214,11 +192,9 @@
 
 			<ul class="main-menu-m">
 				<li>
-<<<<<<< HEAD
+
 					<a href="{{route('index')}}">Home</a>
-=======
-					<a href="index.html">Home</a>
->>>>>>> main
+
 					<ul class="sub-menu-m">
 						<li><a href="index.html">Homepage 1</a></li>
 						<li><a href="home-02.html">Homepage 2</a></li>
@@ -230,11 +206,9 @@
 				</li>
 
 				<li>
-<<<<<<< HEAD
+
 					<a href="{{route('product')}}">Shop</a>
-=======
-					<a href="product.html">Shop</a>
->>>>>>> main
+
 				</li>
 
 				<li>
@@ -242,7 +216,7 @@
 				</li>
 
 				<li>
-<<<<<<< HEAD
+
 					<a href="{{route('blog')}}">Blog</a>
 				</li>
 
@@ -252,17 +226,7 @@
 
 				<li>
 					<a href="{{route('contact')}}">Contact</a>
-=======
-					<a href="blog.html">Blog</a>
-				</li>
 
-				<li>
-					<a href="about.html">About</a>
-				</li>
-
-				<li>
-					<a href="contact.html">Contact</a>
->>>>>>> main
 				</li>
 			</ul>
 		</div>
