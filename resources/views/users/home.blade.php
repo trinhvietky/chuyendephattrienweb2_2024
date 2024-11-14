dfccb330171f3f2041dfd61a350001bbfdbeadf0
maitrananhtuan <maituan5540@gmail.com>
2024-11-14 13:19:07 +0700

    update file

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -17,21 +17,6 @@
 
 		<div class="header-cart-content flex-w js-pscroll">
 			<ul class="header-cart-wrapitem w-full">
-				<li class="header-cart-item flex-w flex-t m-b-12">
-					<div class="header-cart-item-img">
-						<img src="/images/item-cart-01.jpg" alt="IMG">
-					</div>
-
-					<div class="header-cart-buttons flex-w w-full">
-						<a href="{{route('shoping-cart')}}" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
-							View Cart
-						</a>
-
-						<span class="header-cart-item-info">
-							1 x $19.00
-						</span>
-					</div>
-				</li>
 
 				<li class="header-cart-item flex-w flex-t m-b-12">
 					<div class="header-cart-item-img">
