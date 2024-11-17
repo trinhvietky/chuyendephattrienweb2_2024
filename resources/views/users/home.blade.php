fccada5f1e4c6103b25479578e106c78167f3421
trinhvietky <161665737+trinhvietky@users.noreply.github.com>
2024-11-17 12:42:27 +0700

    commit file no change

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -540,6 +540,9 @@
 		<div class="flex-c-m flex-w w-full p-t-45">
 			<a href="{{ route('product') }}" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
 				Show all product
+
+
+				
 			</a>
 		</div>
 	</div>
--- resources/views/users/product-detail.blade.php
+++ resources/views/users/product-detail.blade.php
@@ -343,7 +343,7 @@
 
 	<div class="bg6 flex-c-m flex-w size-302 m-t-73 p-tb-15">
 		<span class="stext-107 cl6 p-lr-25">
-			SKU: JAK-01
+			SKU: JAK-01   
 		</span>
 
 		<span class="stext-107 cl6 p-lr-25">
