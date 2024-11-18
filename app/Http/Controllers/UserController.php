8bd664a5eef67457f66683368bdc8ffbfa7a2b9b
maitrananhtuan <maituan5540@gmail.com>
2024-11-18 15:17:14 +0700

    sua route, csdl

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,12 +6,26 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\Product;
+use App\Models\ProductImage;
 
 class UserController extends Controller
 {
+    public function getProducts()
+    {
+        // Lấy tất cả sản phẩm từ database
+        $products = Product::orderBy('created_at', 'desc')->paginate(10);
+        $images = [];
+
+        foreach ($products as $product) {
+            $images[] = ProductImage::where('product_id', $product->product_id)->first();
+        }
+
+        return compact('products', 'images');
+    }
     public function index()
     {
-        return view('users/home');
+        $data = $this->getProducts();
+        return view('users/home', $data);
     }
 }
