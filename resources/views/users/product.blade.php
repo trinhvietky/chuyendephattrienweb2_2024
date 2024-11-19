e52a0d88298dbfeabf658ed436f6b97a7c333ae8
trinhvietky <161665737+trinhvietky@users.noreply.github.com>
2024-11-19 16:10:05 +0700

    update lan 7

--- resources/views/users/product.blade.php
+++ resources/views/users/product.blade.php
@@ -255,7 +255,7 @@
 					<div class="block2-pic hov-img0">
 						<img src="{{ $images[$index]->image_path }}" alt="IMG-PRODUCT">
 
-						<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+						<a href="{{ route('users/product-detail', ['product_id' => $product->product_id]) }}" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04">
 							Quick View
 						</a>
 					</div>
